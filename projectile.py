1f4047fe47339a9adbce837e17d92f7d754d0165
TopoPhillMan <88756720+TopoPhillMan@users.noreply.github.com>
2022-06-09 21:31:42 -0700

    9:30_6-9-2022

--- projectile.py
+++ projectile.py
@@ -2,22 +2,37 @@ import math
 from tkinter import Y
 
 class projectileMotion():
-    def fTime(angle, velocity, accelorationY, hight):
+
+    # Prereq
+    def xVelocity(velocity, angle):
+        xVelocity = velocity*(math.cos(math.radians(angle)))
+        return xVelocity
+
+    def yVelocity(velocity, angle):
         yVelocity = velocity*(math.sin(math.radians(angle)))
+        return yVelocity
+
+    def fTime(angle, velocity, accelorationY, hight):
+        yVelocity = yVelocity(velocity, angle)
         if(hight == 0 or hight > 0):
             time = (-1*(yVelocity) - math.sqrt(pow(yVelocity, 2) + -4*hight*(0.5*(accelorationY))))/accelorationY
         elif(hight < 0):
             time = (-1*(yVelocity) + math.sqrt(pow(yVelocity, 2) + -4*hight*(0.5*(accelorationY))))/accelorationY
         return time
-    # Angle, Velocity, Gravitational Acceloration, Initial Hight
+
+    # Actual
     def xMax(angle, velocity, accelorationY, hight):
-        xVelocity = velocity*(math.cos(math.radians(angle)))
+        xVelocity = xVelocity(velocity, angle)
         hangTime = projectileMotion.fTime(angle, velocity, accelorationY, hight)
         return((xVelocity)*(hangTime))
-    def yMax(angle, velocity, accelorationY, hight):
-        hangTime = projectileMotion.fTime(angle, velocity, accelorationY, 0)
 
-print(projectileMotion.xMax(20, 40, -9.8, 0))
+    def yMax(angle, velocity, accelorationY, hight):
+        yVelocity = yVelocity(velocity, angle)
+        hangTime = projectileMotion.fTime(angle, velocity, accelorationY, 0)
+        yMax = hight + (yVelocity*hangTime) + (0.5*9.8*pow(hangTime, 2))
+        return yMax
+
+print(projectileMotion.yMax(20, 40, -9.8, 30))
 
 # yVelocity = 13.68080573
 # accelorationY = -9.8
